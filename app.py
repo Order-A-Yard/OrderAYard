e67192679b8c6d46a133237b2e11962c994e799b
Ethan.admin <2304353@leedstrinity.ac.uk>
2026-02-10 12:28:18 +0000

    editing the app to work more with the app, lobby and host page

--- app.py
+++ app.py
@@ -1,83 +1,91 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# from pymongo import MongoClient
-# from bson.objectid import ObjectId
-# from bson.errors import InvalidId
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# # MongoDB configuration
-# client = MongoClient("mongodb+srv://<db_username>:<db_password>@orderayard.pv0zwkr.mongodb.net/")
-# db = client["ServerData"]
-# game = db["game"]
-# lobbyusers = db["lobbyusers"]
-# players = db["players"]
-# sessionusers = db["sessionusers"]
-# # {{ url_for('foldername', filename='file')}}
-# # 1. routes for separate pages
-# @app.route('/')
-# def main():
-#     return render_template('mainpage.html')
+# MongoDB configuration
+client = MongoClient("mongodb+srv://<db_username>:<db_password>@orderayard.pv0zwkr.mongodb.net/")
+db = client["OrderAYard"]
+game = db["game"]
+lobbyusers = db["lobbyusers"]
+players = db["players"]
+sessionusers = db["sessionusers"]
 
-# @app.route('/gamepage')
-# def game():
-#     return render_template('gamePage.html')
+# {{ url_for('foldername', filename='file')}}
+# 1. routes for separate pages
+@app.route('/')
+def main():
+    return render_template('mainpage.html')
 
-# @app.route('/hostgame')
-# def accohostunt():
-#     return render_template('hostGame.html')
+@app.route('/gamepage')
+def gamePage():
+    return render_template('gamePage.html')
 
-# @app.route('/joingame')
-# def Join():
-#     return render_template('joinGame.html')
+@app.route('/hostgame', methods=['GET', 'POST'])
+def accohostunt():
+    if request.method == 'POST':
+        data = request.get_json()
+        players.insert_one({
+            "name": data["name"],
+            "color": data["color"]
+        })
+        return {"status": "ok"}
+    return render_template('hostGame.html')
+
+@app.route('/joingame')
+def Join():
+    return render_template('joinGame.html')
 
 
 
-# @app.route('/lobbypage', methods=['GET', 'POST', 'PUT', 'DEL', 'PATCH'])
-# def lobby():
+@app.route('/lobbypage', methods=['GET', 'POST', 'PUT', 'DEL', 'PATCH'])
+def lobby():
+    all_players = list(players.find())
+    
+    return render_template('lobbyPage.html', players=all_players)
    
-#     if request.method == 'POST':
+    # if request.method == 'POST':
 
-#     #USING LATER WHEN INFORMATION CAN BE ASSIGNED
-#         # inputUserData = {
-#     #         "username": request.form['username'],
-#     #         "imageId": request.form['imageId'],
-#     #         "email": request.form['email'],
-#     #         "password": request.form['password'],
-#     #         "conpassword": request.form['conpassword'],
-#     #     }
-#     #     userData.insert_one(inputUserData)
-#     #     # return redirect(url_for('login'))
-#     # return render_template('page1.html')
+    #USING LATER WHEN INFORMATION CAN BE ASSIGNED
+        # inputUserData = {
+    #         "username": request.form['username'],
+    #         "imageId": request.form['imageId'],
+    #         "email": request.form['email'],
+    #         "password": request.form['password'],
+    #         "conpassword": request.form['conpassword'],
+    #     }
+    #     userData.insert_one(inputUserData)
+    #     # return redirect(url_for('login'))
+    # return render_template('page1.html')
 
-#     #     email = request.form['email']
-#     #     password = request.form['password']
-#     #     action = request.form['action']
-#     #     if action == 'signup':
-#     #         inputUserData = {
-#     #         "username": request.form['username'],
-#     #         # "imageId": request.form['imageId'],
-#     #         "email": email,
-#     #         "password": password,
-#     #         "conpassword": request.form['conpassword'],
-#     #         }
-#     #         userData.insert_one(inputUserData)
-#     #         return redirect(url_for('login'))
-#     #     else:
-#     #         user = userData.find_one({"email": email, "password": password})
-#     #         if user:
-#     #             return redirect (url_for('login'))
-#     # return render_template('page1.html')
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     action = request.form['action']
+    #     if action == 'signup':
+    #         inputUserData = {
+    #         "username": request.form['username'],
+    #         # "imageId": request.form['imageId'],
+    #         "email": email,
+    #         "password": password,
+    #         "conpassword": request.form['conpassword'],
+    #         }
+    #         userData.insert_one(inputUserData)
+    #         return redirect(url_for('login'))
+    #     else:
+    #         user = userData.find_one({"email": email, "password": password})
+    #         if user:
+    #             return redirect (url_for('login'))
+    # return render_template('page1.html')
+    
 
 
-# @app.route('/videos')
-# def videos():
-#     return render_template('videos.html')
-
-# @app.route('/websites')
-# def websites():
-#     return render_template('websites.html')
-
+@app.route('/api/players')
+def get_players():
+    all_players = list(players.find({}, {"_id": 0}))
+    return jsonify(all_players)
 
     
-# if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug=True)
