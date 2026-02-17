e6b5279b5afbea475e938b509ba94477d67083a2
2302616@leedstrinity.ac.uk <2302616@leedstrinity.ac.uk>
2026-02-17 14:48:49 +0000

    hostgame js created, game ID is now displayed on lobby page when host creates a game

--- lobbyPage.js
+++ lobbyPage.js
@@ -1,13 +1,3 @@
-document.getElementById('readyButton').addEventListener('click', function() {
-    fetch('http://trinity-developments.co.uk/games', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            name: "JS Test POST2",
-            mapId: 1,
-            gameLength: "short"
-        })   
-    })
-    .then (res => res.json())
-    .then (data => {window.location.href = "/gamePage.html"})
-})
+const gameId = localStorage.getItem('gameId');
+
+document.getElementById('gameCodeDisplay').textContent = gameId;
